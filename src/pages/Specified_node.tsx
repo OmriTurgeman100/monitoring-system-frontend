68789f2cf2becd8c8d09c47226c764cf46cbbc31
archery <omrigamer100@gmail.com>
2024-10-21 19:30:36 +0300

    test

--- src/pages/Specified_node.tsx
+++ src/pages/Specified_node.tsx
@@ -1,7 +1,29 @@
 import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useState } from "react";
 
 export const Specified_node = () => {
   const { id } = useParams();
+
+  const get_data = async () => {
+    const response = await fetch(`http://127.0.0.1/api/v1/nodes/${id}`);
+
+    const data = await response.json();
+
+    if (data.reports && data.reports.length > 0) {
+      console.log("reports");
+    }
+    
+
+    if (data.nodes && data.nodes.length > 0) {
+      console.log("nodes");
+    }
+    
+  };
+
+  useEffect(() => {
+    get_data();
+  }, []);
 
   return (
     <div>
