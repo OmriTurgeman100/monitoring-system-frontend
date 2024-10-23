b47512680294f6fe20cde1b4f775221ce969ef7d
archery <omrigamer100@gmail.com>
2024-10-23 15:32:43 +0300

    test

--- src/components/PostReport.tsx
+++ src/components/PostReport.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
+import Button from "@mui/material/Button";
+import "../styles/post_report.css";
 
 interface report {
   description: string;
@@ -29,11 +32,28 @@ export const PostReport = () => {
   useEffect(() => {
     fetch_reports();
     console.log(reports);
-  }, []);
+  }, [reports]);
 
   return (
-    <div>
-      <h1>post report</h1>
+    <div className="report_container">
+      <input></input> 
+      {reports.map((report) => (
+        <div className="report">
+          <Button
+            variant="contained"
+            size="small"
+            sx={{
+              minWidth: "20px", // Custom width
+              padding: "2px 6px", // Custom padding
+              fontSize: "10px", // Smaller font size
+            }}
+          >
+            <AddOutlinedIcon sx={{ fontSize: "16px" }} />{" "}
+            {/* Smaller icon size */}
+          </Button>
+          <h3>{report.title}</h3>
+        </div>
+      ))}
     </div>
   );
 };
