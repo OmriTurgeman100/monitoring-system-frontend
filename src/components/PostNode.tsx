d56a8dc6c257c4778dd71a915833857d72236e12
archery <omrigamer100@gmail.com>
2024-10-23 12:00:23 +0300

    added submit form event for sending nodes.

--- src/components/PostNode.tsx
+++ src/components/PostNode.tsx
@@ -1,20 +1,29 @@
 import Button from "@mui/material/Button";
+import { FormEvent, useState } from "react";
 import "../styles/post_nodes.css";
 
 export const PostNode = () => {
-  const handle_submit = (event: any) => {
+  const [name, setName] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+
+  const handle_submit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     console.log("Form submitted");
+    console.log(`name is ${name}`);
+    console.log(`description is ${description}`);
   };
 
   return (
     <div className="post_node_layout">
       <form onSubmit={handle_submit}>
         <label> Name </label>
-        <input type="text" name="name" />
+        <input onChange={(e) => setName(e.target.value)} value={name} />
 
         <label> Description </label>
-        <input type="text" name="description" />
+        <input
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+        />
 
         <Button sx={{ marginTop: "10px" }} type="submit" variant="contained">
           Submit
