eed551d50fa53750b01f1f19d3e1f690acb4c9a0
archery <omrigamer100@gmail.com>
2024-10-25 00:59:54 +0300

    styling the rules page

--- src/components/Get_Node_Rules.tsx
+++ src/components/Get_Node_Rules.tsx
@@ -1,17 +1,37 @@
 import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import "../styles/get_node_rules.css";
 
 export const Get_Node_Rules = () => {
   const { id } = useParams();
   const navigate = useNavigate();
 
+  const [rules, setRules] = useState<any>([])
+
+  const fetch_node_rules = async () => {
+    const response = await fetch(
+      `http://localhost/api/v1/get/node/rules/${id}`
+    );
+
+    const data = await response.json();
+
+    console.log(data);
+
+    if (response.ok) {
+        setRules(data)
+    }
+  };
+
   useEffect(() => {
-    console.log("hey from use effect!");
+    fetch_node_rules();
   }, []);
 
   return (
     <div>
-      <h1>get node rules</h1>
+      <div className="node_rules_container">
+        
+
+      </div>
     </div>
   );
 };
