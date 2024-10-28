ae6b90d9834c1c6b325aff7ac3a7cc26a4b06e1b
archery <omrigamer100@gmail.com>
2024-10-28 14:11:22 +0200

    added call to backend

--- src/components/TimeSeriesGraph.tsx
+++ src/components/TimeSeriesGraph.tsx
@@ -4,9 +4,7 @@ import { useEffect, useState } from "react";
 export const TimeSeriesGraph = () => {
   const { id } = useParams();
 
-  const fetch_graph_data = async () => {
-    
-  }
+  const fetch_graph_data = async () => {};
 
   useEffect(() => {
     console.log("hello from the graph page");
@@ -15,8 +13,6 @@ export const TimeSeriesGraph = () => {
   return (
     <div>
       <h2>{id}</h2>
-
-     
     </div>
   );
 };
