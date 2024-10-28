3e940241faf4661608010abb2d147e77975dfcfc
archery <omrigamer100@gmail.com>
2024-10-28 13:59:33 +0200

    added graph function

--- src/components/TimeSeriesGraph.tsx
+++ src/components/TimeSeriesGraph.tsx
@@ -4,6 +4,10 @@ import { useEffect, useState } from "react";
 export const TimeSeriesGraph = () => {
   const { id } = useParams();
 
+  const fetch_graph_data = async () => {
+    
+  }
+
   useEffect(() => {
     console.log("hello from the graph page");
   }, []);
